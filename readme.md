d3bcb288d91320e57396ddcab3232df39276b37a
bellaisafranco <isabella.franco@edu.sait.ca>
2024-11-28 21:54:12 -0700

    i think it works

---
+++ readme.md
@@ -0,0 +1,14 @@
+Attributions and Citations
+
+“Fiona Apple cassette :-)” 2011 by Jesus Rodriguez jmrodri, https://www.flickr.com/photos/33205801@N00/5969956475. Licensed under CC BY 2.0, https://creativecommons.org/licenses/by/2.0/.
+
+"Fiona Apple with a cheque from Sony cira 1995" by Fiona Apple (assets), https://www.flickr.com/photos/fiona-apple/. Licensed under CC BY 2.0, https://creativecommons.org/licenses/by/2.0/.
+
+"This was a great show! Not what I was expecting, but awesome! And now I can say I've seen Fiona Apple live. Finally. After loving her since I was about 15 years old." by aislinn2000, https://www.flickr.com/photos/40431915@N00/10303406154. Licensed under CC BY-ND 2.0,
+https://creativecommons.org/licenses/by-nd/2.0/.
+
+"Fiona Apple eye makeup, 2020" by Fiona Apple (assets), https://www.flickr.com/photos/189585330@N03/50191713197. Licensed under CC BY 2.0, https://creativecommons.org/licenses/by/2.0/.
+
+"Fiona Apple - Oh Well" by tophrrrr, https://www.flickr.com/photos/11317612@N03/21479188733. Licensed under CC BY 2.0, https://creativecommons.org/licenses/by/2.0/.
+
+"Fiona Apple (cropped)" by jareed, https://commons.wikimedia.org/w/index.php?curid=80436603. Licensed under CC BY 2.0, https://creativecommons.org/licenses/by/2.0/.
--- scripts.js
+++ scripts.js
@@ -19,11 +19,42 @@ wtp.addEventListener('click', function(){
     cappy.innerHTML = "'This was a great show! Not what I was expecting, but awesome! And now I can say Ive seen Fiona Apple live. Finally. After loving her since I was about 15 years old.' by aislinn2000 on Flickr, under CC BY-ND 2.0."
 })
 em.addEventListener('click', function(){
-    ogh2.innerHTML = "Extraordinary Machine and the protest";
+    ogh2.innerHTML = "Extraordinary Machine And The Apple Protest";
     intro.innerHTML = "After an incredible 2 year run Fiona Apple seemed to have fallen off the face of the earth, after her album Extraordinary Machine produced by Jon Brion was leaked on a fansite, fans began protesting and sending apples to Sony in an effort to pressure them into releasing the perfect album. After the Jon Brion leaks the album was completely redone with another producer. Fiona Has criticized this album for the inclusion of 'bs' filler songs that she was forced to create under the terms of her contract. That being said this album brought Fiona Apple into the 2000s, with a new more mature sound and her writing sharp as ever."
     oggh2.innerHTML = "Looking for a song with a music video directed by Paul Thomas Anderson and starring Zack Galafianakis?"
     songsug.innerHTML = "Not About Love ... 4:19"
     console.log("im trying")
     document.getElementById('fifipic').src = "oh-well.jpg"
     document.getElementById('fifipic').alt = " Messy writing of the lyrics, 'What you did to me made me see myself something awful.' from the song 'Oh Well' by Fiona Apple."
-    cappy.innerHTML = '"Fiona Apple - Oh Well" by tophrrrr, on Flickr under CC BY 2.0'})
+    cappy.innerHTML = '"Fiona Apple - Oh Well" by tophrrrr, on Flickr under CC BY 2.0'
+})
+wheel.addEventListener('click', function(){
+    ogh2.innerHTML = "The Idler Wheel Is Wiser Than the Driver of the Screw and Whipping Cords Will Serve You More Than Ropes Will Ever Do";
+    intro.innerHTML = "Fiona Apple entered the 2010's with an unforgettable album, though the title is too hard to remember. Moving away from the piano backed ballads that marked the begginning of her career Fiona began experimenting with percussion. This album is so different from her debut album that you might make the mistake of thinking they are by two different artists. The change in music driven by maturity and life experience continues to showcase her phenominal writing ability as well as her knack for using instruments when words dont't cut it. With lyrics like 'I ran out of white dove feathers to soak up the hot piss that comes from your mouth everytime you address me' and 'My heart's made of parts of all that surround me and that's why the devil just can't get around me.' it is easy to see Fiona is a once in a lifetime artist."
+    oggh2.innerHTML = "Seriously Listen To The Whole Album, But If I Have To Pick One:"
+    songsug.innerHTML = "Left Alone ... 4:50"
+    console.log("im so tired")
+    document.getElementById('fifipic').src = "fiona-apple-2005.jpg"
+    document.getElementById('fifipic').alt = "Fiona Apple singing passionately on stage."
+    cappy.innerHTML = '"Fiona Apple (cropped)" by Jareed, on Wikimedia.org under CC BY 2.0'
+})
+ftbc.addEventListener('click', function(){
+    ogh2.innerHTML = "Fetch The Bolt Cutters, Worth The Wait";
+    intro.innerHTML = "We Finally find ourselves caught up in the Fiona Apple timeline, after a 9 year wait Fiona releases an album that she recorded a majority of in her home on garageband, released a month before lockdown. Fiona proved herself a poet and possibly a prophet. In all seriousness this Album took the world by storm, earning the first PitchFork 10/10 rating in over a decade (mind you, Kanye West, the last recepient of this review stated his admiration of Fiona Apple and her Writing ability and her impressive lexicon). Fiona Apple scored 2 Grammy's with this Album, skipped the Grammy's (Never forget the ostracized her at 19) and proceeds to live a quiet life out of the spotlight. She appears to her fans through a single fan account/archive and her roommate, Zelda, who recently reported Fiona is writing new music and feeling very inspired. She spends her days walking her dog Mercy, staying diligent in Court Watcher volunteer groups, occaisionally going online to discuss her latest obsession (most notably the time she spoke at length on her love of slugs) and one day maybe this year or five years from now she will release another stunning album that moves all it's listeners."
+    oggh2.innerHTML = "Must Listen:"
+    songsug.innerHTML = "Ladies ... 5:25"
+    console.log("im so close pls let me be done i have slept in 2 days")
+    document.getElementById('fifipic').src = "fiona-apple-2020.jpg"
+    document.getElementById('fifipic').alt = "Fiona Apple with messy eyeliner posing with her dog, Mercy."
+    cappy.innerHTML = '"Fiona Apple eye makeup, 2020" by Fiona Apple (assets), on Flickr under CC BY 2.0.'
+})
+fa101.addEventListener('click', function(){
+    ogh2.innerHTML = "First Glance at Fiona Apple"
+    intro.innerHTML = "Fiona Apple has an incredible discography and career in almost 30 years as a recording artist. From getting blacklisted as a teenager at the Grammy's to her efforts in court watching and human rights advocacy during the pandemic, she's done it all and along the way created incredibly beautiful music. This site will take you through the timeline of her career spanning four decades and five albums."
+    oggh2.innerHTML = "First Taste of Fiona Apple? Start Here!"
+    songsug.innerHTML = "Under The Table ... 3:21"
+    console.log("pls work")
+    document.getElementById('fifipic').src = "fiona-apple-1995.jpg"
+    document.getElementById('fifipic').alt = "A young Fiona Apple holding a cheque from sony in 1995"
+    cappy.innerHTML ='"Fiona Apple with a cheque from Sony cira 1995" by Fiona Apple (assets) on Flickr under CC BY 2.0'
+})
