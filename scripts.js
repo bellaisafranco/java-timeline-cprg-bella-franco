5778f800a6d2adf4f11146a2f206cb938fc42445
bellaisafranco <isabella.franco@edu.sait.ca>
2024-11-28 20:53:24 -0700

    i make a da java

---
+++ scripts.js
@@ -0,0 +1,30 @@
+tidal.addEventListener('click', function(){
+    ogh2.innerHTML = "Tidal, The Debut Album of All Time";
+    intro.innerHTML = "Fiona Apple came onto the music scene with smash hit 'Criminal' a song she wrote on her lunch break at 17 years old when her label demanded a single for the album. She literally won a Grammy for a song she wrote as a teenager... in under an hour. Don't worry the song still holds up, along with the rest of the Album. Fiona Apple was a staple of the late 90's, she was often found on the various sets of Paul Thomas Anderson or wearing angel wings on stage."
+    oggh2.innerHTML = "Best Song on Tidal?"
+    songsug.innerHTML = "Carrion ... 5:46"
+    console.log("ahhh1")
+    document.getElementById('fifipic').src = "tidal.jpg"
+    document.getElementById('fifipic').alt = "Cassette tape of Fiona Apple's Album 'Tidal'."
+    cappy.innerHTML = "“Fiona Apple cassette :-)” 2011 by Jesus Rodriguez, jmrodri on Flickr under CC BY 2.0"
+})
+wtp.addEventListener('click', function(){
+    ogh2.innerHTML = "When The Pawn... Longest Album Title";
+    intro.innerHTML = "Released in 1999 Fiona Apple solidified her as a star an gifted songwriter. This sophmore album is featured on Rolling Stones top 100 albums of the 90's, mentioned in Kurt Kobain's diary and beloved by all Fiona fans. When The Pawn... boasts a 90 word title making it memorable before you even listen. "
+    oggh2.innerHTML = "Which Pawn?"
+    songsug.innerHTML = "A Mistake ... 4:56"
+    console.log("okayyy")
+    document.getElementById('fifipic').src = "fiona-apple-show.jpg"
+    document.getElementById('fifipic').alt = "Picture of a theatre sign advertising a Fiona Apple concert."
+    cappy.innerHTML = "'This was a great show! Not what I was expecting, but awesome! And now I can say Ive seen Fiona Apple live. Finally. After loving her since I was about 15 years old.' by aislinn2000 on Flickr, under CC BY-ND 2.0."
+})
+em.addEventListener('click', function(){
+    ogh2.innerHTML = "Extraordinary Machine and the protest";
+    intro.innerHTML = "After an incredible 2 year run Fiona Apple seemed to have fallen off the face of the earth, after her album Extraordinary Machine produced by Jon Brion was leaked on a fansite, fans began protesting and sending apples to Sony in an effort to pressure them into releasing the perfect album. After the Jon Brion leaks the album was completely redone with another producer. Fiona Has criticized this album for the inclusion of 'bs' filler songs that she was forced to create under the terms of her contract. That being said this album brought Fiona Apple into the 2000s, with a new more mature sound and her writing sharp as ever."
+    oggh2.innerHTML = "Looking for a song with a music video directed by Paul Thomas Anderson and starring Zack Galafianakis?"
+    songsug.innerHTML = "Not About Love ... 4:19"
+    console.log("im trying")
+    document.getElementById('fifipic').src = "fiona-apple-show.jpg"
+    document.getElementById('fifipic').alt = "Picture of a theatre sign advertising a Fiona Apple concert."
+    cappy.innerHTML = "'This was a great show! Not what I was expecting, but awesome! And now I can say Ive seen Fiona Apple live. Finally. After loving her since I was about 15 years old.' by aislinn2000 on Flickr, under CC BY-ND 2.0."
+})
